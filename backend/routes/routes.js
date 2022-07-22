77b10b9f142f1233cba1152cc413a23dc82843bb
geniusfaker <geniusfaker@gmail.com>
2022-07-22 09:50:17 +0200

    hi

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,16 +3,11 @@ const multer = require("multer");
 const router = express.Router();
 const API = require("../controllers/api");
 
-const multerStorage = multer.memoryStorage();
 const Storage = API.Storage;
 
-const upload = multer({
-  storage: Storage,
-}).single("image");
-
 router.get("/", API.fetchAllAgents);
-router.post("/", upload, API.createAgent);
-router.patch("/:id", upload, API.updateAgent);
+router.post("/", API.createAgent);
+router.patch("/:id", API.updateAgent);
 router.delete("/:id", API.deleteAgent);
 router.get("/:id", API.fetchAgentById);
 

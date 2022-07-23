d619f09662b8688266e692d3ba4087b5fbd6e77f
geniusfaker <geniusfaker@gmail.com>
2022-07-23 15:55:14 +0200

    hi

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+      <MenuComponent />
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import MenuComponent from '../components/MenuComponent.vue';
+
+const leftDrawerOpen = ref(true);
+const rightDrawerOpen = ref(true);
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
+
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
+</script>

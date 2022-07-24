e62cacae579fe4ac4d3b0ca15c82e58062997234
geniusfaker <geniusfaker@gmail.com>
2022-07-25 01:39:19 +0200

    hi

--- frontend/src/pages/AgentsPage.vue
+++ frontend/src/pages/AgentsPage.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="q-pa-md">
-    <q-carousel
-      v-model="slide"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      swipeable
-      animated
-      control-color="primary"
-      navigation
-      padding
-      arrows
-      height="300px"
-      class="bg-grey-1 shadow-2 rounded-borders"
-    >
+
       <AgentCard />
     </q-carousel>
   </div>
